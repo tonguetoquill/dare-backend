0f56ef3e687d59331d2c7500354ebb62171b5598
farhat-is-coding <farhxt.abbxs69@gmail.com>
2025-03-13 18:09:34 +0500

    feat: dashboard stats

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 
+from users.api.views import UserStatsView
 from users.constants import APP_NAME
 
 app_name = APP_NAME
@@ -7,4 +8,6 @@ app_name = APP_NAME
 urlpatterns = [
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("stats/", UserStatsView.as_view(), name="user-stats"),
+
 ]
