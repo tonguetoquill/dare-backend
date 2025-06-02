2faada509f8dbfb0356bc68e536e8983f5fb853e
Muhammad Abdurrehman <abdurrehman.user.95@gmail.com>
2025-06-02 20:33:35 +0500

    fix: minor change

--- core/scheduler_runner.py
+++ core/scheduler_runner.py
@@ -5,7 +5,7 @@ import logging
 import django
 
 # Configure Django
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
